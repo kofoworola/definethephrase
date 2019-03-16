ff5f207d7e8d1e1498c95f474c69289b0b24b930
Kofo Okesola <okesolakofo@gmail.com>
2019-03-16 09:15:05 +0100

    crc check and webhook registration

---
+++ redis/redis_test.go
@@ -0,0 +1,29 @@
+package redis
+
+import (
+	"github.com/gomodule/redigo/redis"
+	"testing"
+)
+
+func TestConnection(t *testing.T){
+	conn := GetPool().Get()
+	defer conn.Close()
+	resp,err := redis.String(conn.Do("PING"))
+	if err != nil || resp != "PONG"{
+		t.Error("Expected PONG got",resp)
+	}
+}
+
+func TestGetValue(t *testing.T){
+	conn := GetPool().Get()
+	defer conn.Close()
+	_,err := conn.Do("SET","test_value","Hello world")
+	if err != nil{
+		t.Error("An error occurred when setting value",err.Error())
+		return
+	}
+	resp,err := redis.String(conn.Do("GET","test_value"))
+	if err != nil || resp != "Hello world"{
+		t.Error("Expected 'Hello world' got",resp)
+	}
+}
--- twitter/listener.go
+++ twitter/listener.go
@@ -9,24 +9,27 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
-
-var TWITTER_SECRET = os.Getenv("TWITTER_SECRET")
-var TWITTER_API_KEY = os.Getenv("TWITTER_API_KEY")
 
 type crcResponse struct {
 	ResponseToken string `json:"response_token"`
 }
+
 func CrcCheck(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type","application/json")
+	fmt.Printf("Crc check occurred at %s\n",time.Now().String())
 	token := request.URL.Query()["crc_token"]
 	if len(token) < 1 {
 		fmt.Println("No crc_token given")
 		fmt.Fprintf(writer, "No token given")
 		return
 	}
-	h := hmac.New(sha256.New,[]byte(TWITTER_SECRET))
+	h := hmac.New(sha256.New, []byte(os.Getenv("CONSUMER_SECRET")))
 	h.Write([]byte(token[0]))
+	fmt.Println("Secret key is "+ os.Getenv("CONSUMER_SECRET"))
+	fmt.Println("token is " + token[0])
 	encoded := base64.StdEncoding.EncodeToString([]byte(hex.EncodeToString(h.Sum(nil))))
-	response,_ := json.Marshal(crcResponse{ResponseToken:"sha256="+encoded})
-	fmt.Fprintf(writer,string(response))
+	response, _ := json.Marshal(crcResponse{ResponseToken: "sha256=" + encoded})
+	fmt.Fprintf(writer, string(response))
 }
